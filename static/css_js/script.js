6b432c6bcb86fe3779c6fa166d6e821facc7ff0d
Hiran ODLab GCK <116419781+hiran-odlabGCK@users.noreply.github.com>
2022-11-04 16:09:26 +0530

    Add files via upload

---
+++ static/css_js/script.js
@@ -0,0 +1,103 @@
+var deptObject = {
+  Arabic: {
+    "2021-22": ["SAMPLE1", "SAMPLE2", "SAMPLE3"],
+    "2020-21": ["SAMPLE1", "SAMPLE2", "SAMPLE3"],
+    "2019-20": ["SAMPLE1", "SAMPLE2", "SAMPLE3"],
+    "2018-19": ["SAMPLE1", "SAMPLE2", "SAMPLE3"],
+    "2017-18": ["SAMPLE1", "SAMPLE2", "SAMPLE3"],
+  },
+  Economics: {
+    "2021-22": [
+      "Dr. HARI KURUP K K",
+      "Dr. REJUNA C A",
+      "Mr. BALAKRISHNAN U",
+      "Mr. SUBHASH P P",
+      "Mr. ISMAYIL P",
+      "Mr. ANOOPKUMAR M",
+      "Mr. VINOD V",
+    ],
+    "2020-21": [
+      "Dr. HARI KURUP K K",
+      "Dr. REJUNA C A",
+      "Mr. BALAKRISHNAN U",
+      "Mr. SUBHASH P P",
+      "Mr. ISMAYIL P",
+      "Mr. ANOOPKUMAR M",
+      "Mr. VINOD V",
+    ],
+    "2019-20": [
+      "Dr. HARI KURUP K K",
+      "Dr. REJUNA C A",
+      "Mr. BALAKRISHNAN U",
+      "Mr. SUBHASH P P",
+      "Mr. ISMAYIL P",
+      "Mr. ANOOPKUMAR M",
+      "Mr. VINOD V",
+    ],
+    "2018-19": [
+      "Dr. HARI KURUP K K",
+      "Dr. REJUNA C A",
+      "Mr. BALAKRISHNAN U",
+      "Mr. MANOJ CHATHOTH",
+      "Mr. SUBHASH P P",
+      "Mr. ISMAYIL P",
+      "Mr. ANOOPKUMAR M",
+    ],
+    "2017-18": [
+      "Dr. HARI KURUP K K",
+      "Dr. REJUNA C A",
+      "Mr. BALAKRISHNAN U",
+      "Mr. MANOJ CHATHOTH",
+      "Mr. SUBHASH P P",
+      "Mr. ISMAYIL P",
+      "Mr. ANOOPKUMAR M",
+    ],
+  },
+};
+
+var deptSel = document.getElementById("department");
+var yearSel = document.getElementById("year");
+var facultySel = document.getElementById("faculty");
+
+window.onload = function () {
+  for (var x in deptObject) {
+    deptSel.options[deptSel.options.length] = new Option(x, x);
+  }
+  deptSel.onchange = function () {
+    //empty faculty- and year- dropdowns
+    facultySel.length = 1;
+    yearSel.length = 1;
+    //display correct values
+    for (var y in deptObject[this.value]) {
+      yearSel.options[yearSel.options.length] = new Option(y, y);
+    }
+  };
+  yearSel.onchange = function () {
+    //empty faculty dropdown
+    facultySel.length = 1;
+    //display correct values
+    var z = deptObject[deptSel.value][this.value];
+    for (var i = 0; i < z.length; i++) {
+      facultySel.options[facultySel.options.length] = new Option(z[i], z[i]);
+    }
+  };
+};
+
+
+document.getElementById("appointmentBtn").onclick = function () {
+  if (facultySel.value === "Mr. ANOOPKUMAR M") {
+    document.getElementById("appointmentBtn").href = "/home/user/Desktop/Hiran/economics/appointment order/AnoopKumar.pdf";
+  }
+  else  {
+    document.getElementById("appointmentBtn").href = "/home/user/Desktop/Hiran/dummy.pdf"
+  }
+};
+
+document.getElementById("joiningBtn").onclick = function () {
+  if (facultySel.value === "Mr. ANOOPKUMAR M") {
+    document.getElementById("joiningBtn").href = "/home/user/Desktop/Hiran/economics/joining order/Anoop  joining order.pdf";
+  }
+  else  {
+    document.getElementById("joiningBtn").href = "/home/user/Desktop/Hiran/dummy.pdf"
+  }
+};
